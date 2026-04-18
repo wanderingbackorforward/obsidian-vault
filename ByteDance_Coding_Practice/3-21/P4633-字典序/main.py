993209ef2e56b5d106a021cdb6aaaba34d39ec3d
ynj <yangn497@gmail.com>
2026-04-19 03:20:31 +0800

    vault backup: 2026-04-19 03:20:31
    Affected files:
    ByteDance_Coding_Practice/3-21/P4633-字典序/main.py

--- ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
+++ ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
@@ -1,5 +1,17 @@
+def c2num(c):
+    return ord(c)-ord('a')
+def to_num(s):
+    res = 0
+    while s:
+        c = s[0]
+        res = res*26+c2num(c)
+        s = s[1:]
+    return res
+
 def solve(n,s1,s2):
-    pass
+    n1 = to_num(s1)
+    n2 = to_num(s2)
+    return s2-s1
 
 def main():
     T = int(input())
