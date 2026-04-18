b6af13b0b1f3e8ba3ccb61bce48aeaa3a3bd9d04
ynj <yangn497@gmail.com>
2026-04-19 03:00:57 +0800

    vault backup: 2026-04-19 03:00:57
    Affected files:
    ByteDance_Coding_Practice/3-21/P4633-字典序/main.py

--- ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
+++ ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
@@ -1,26 +1,6 @@
-def solve_str(x,s):
-    k = x//2
-    s1 = ''.join(sorted(s[:k]))
-    s2 = ''.join(sorted(s[:k],reverse=True))
-    s3 = ''.join(sorted(s[k:]))
-
-    return s1+s2+s3
-
-def solve(n,x,s):
-    st=set()
-    for i in range(n-x+1):
-        new_s1 = s[i:i+x]
-        new_s2 = solve_str(x,new_s1)
-        st.add(new_s2)
-    return len(st)
 
 def main():
-    T = int(input())
-    for _ in range(T):
-        n,x = map(int,input().split())
-        s = input().strip()
-        count = solve(n,x,s)
-        print (count)
+    pass
 
 if __name__ =="__main__":
     main()
