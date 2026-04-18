45eaa2ff0ba7695c3e4c4f2ca84963700c17d0bd
ynj <yangn497@gmail.com>
2026-04-19 01:28:39 +0800

    vault backup: 2026-04-19 01:28:39
    Affected files:
    ByteDance_Coding_Practice/3-21/P4633-字典序/main.py

--- ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
+++ ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
@@ -1,5 +1,25 @@
+def solve_str(x,s):
+    k = x//2
+    s1 = ''.join(sorted(s[:k]))
+    s2 = ''.join(sorted(s[:k],reverse=True))
+    s3 = ''.join(sorted(s[k:]))
+
+    return s1+s2+s3
+
+def solve(n,x,s):
+    s=set()
+    for i in range(n-x+1):
+        new_s1 = s[i,i+x]
+        new_s2 = solve_str(x,new_s1)
+        s.add(new_s2)
+    return len(s)
+
 def main():
-    pass
+    T = int(input())
+    for _ in range(T):
+        n,x = map(int,input())
+        s = input().strip()
+        count = sovle(n,x,s)
 
 if __name__ =="__main__":
     main()
