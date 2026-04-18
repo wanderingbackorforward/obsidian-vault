895661ecaeb38e1a3a36e8ab27ffd49e1a99a56e
ynj <yangn497@gmail.com>
2026-04-19 03:07:42 +0800

    vault backup: 2026-04-19 03:07:42
    Affected files:
    ByteDance_Coding_Practice/3-21/P4633-字典序/main.py

--- ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
+++ ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
@@ -1,6 +1,12 @@
+def solve(n,s1,s2):
+    pass
 
 def main():
-    pass
+    T = int(input())
+    for _ in range(T):
+        n,s1,s2 = input().split()
+        res = solve(n,s1,s2)
+        print(res)
 
 if __name__ =="__main__":
     main()
