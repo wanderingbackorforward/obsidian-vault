7ef4020440b77f5eff9c1c8bd85f1bc3d1afc78c
ynj <yangn497@gmail.com>
2026-04-19 21:02:55 +0800

    vault backup: 2026-04-19 21:02:55
    Affected files:
    ByteDance_Coding_Practice/9-21/P3770-合并序列/main.py

---
+++ ByteDance_Coding_Practice/9-21/P3770-合并序列/main.py
@@ -0,0 +1,12 @@
+
+def solve(n,k):
+    res=[]
+
+    return res
+
+def main():
+    n,k = map(int,input().split())
+    res = solve(n,k)
+    print(' '.join(map(str,str)))
+if __name__ == "__main__":
+    main()
