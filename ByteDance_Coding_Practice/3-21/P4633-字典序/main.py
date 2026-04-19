410291f48b726cec683d1bb4b0d52f6f23c8c2f2
ynj <yangn497@gmail.com>
2026-04-19 14:32:08 +0800

    vault backup: 2026-04-19 14:32:08
    Affected files:
    ByteDance_Coding_Practice/3-21/P4633-字典序/main.py

--- ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
+++ ByteDance_Coding_Practice/3-21/P4633-字典序/main.py
@@ -1,28 +1,16 @@
-def c2num(c):
-    return ord(c)-ord('a')
-def to_num(s):
-    res = 0
-    while s:
-        c = s[0]
-        res = res*26+c2num(c)
-        s = s[1:]
-    return res
-
-def solve(n,s1,s2):
-    if s1==s2:
-        return 0
-    n1 = to_num(s1)
-    n2 = to_num(s2)
-    return n2-n1-1 if n2>=n1 else 0
+def solve(grid):
+    pass
 
 def main():
     T = int(input())
     results = []
     for _ in range(T):
-        n,s1,s2 = input().split()
-        results.append(solve(n,s1,s2))
-    for res in results:
-        print(res)
+        n = int(input())
+        grid = [input().strip() for _ in range(n)]
+        counts,ways=solve(grid)
+        results.append((counts,ways))
+    for counts,ways in results:
+        print(f"{counts} {ways}")
 
 if __name__ =="__main__":
     main()
