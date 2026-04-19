fd07e80df10da64a4683162c9bf8b890897a2e23
ynj <yangn497@gmail.com>
2026-04-19 18:58:05 +0800

    vault backup: 2026-04-19 18:58:05
    Affected files:
    ByteDance_Coding_Practice/9-21/P3769-节点树的简单路径/main.py

--- ByteDance_Coding_Practice/9-21/P3769-节点树的简单路径/main.py
+++ ByteDance_Coding_Practice/9-21/P3769-节点树的简单路径/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def dfs(node):
     pass
 
@@ -5,7 +7,12 @@ def solve():
     pass
 
 def main():
-    pass
+    T = map(int,input())
+    weights = map(int,input().split())
+    uvs = {}
+    for _ in range(T-1):
+        u,v = map(int,input())
+
 
 if __name__ == "__main__":
     main()
