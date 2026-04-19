e1bebb7e99f91f6b244320189265645a66f837e8
ynj <yangn497@gmail.com>
2026-04-19 19:09:20 +0800

    vault backup: 2026-04-19 19:09:20
    Affected files:
    ByteDance_Coding_Practice/9-21/P3769-节点树的简单路径/main.py

--- ByteDance_Coding_Practice/9-21/P3769-节点树的简单路径/main.py
+++ ByteDance_Coding_Practice/9-21/P3769-节点树的简单路径/main.py
@@ -3,16 +3,21 @@ from collections import defaultdict
 def dfs(node):
     pass
 
-def solve():
-    pass
+def solve(weights,uvs):
+    res = []
+
+    return res
 
 def main():
     T = map(int,input())
     weights = map(int,input().split())
-    uvs = {}
+    uvs = defaultdict(list)
     for _ in range(T-1):
         u,v = map(int,input())
-
+        uvs[u-1].append(v-1)
+        uvs[v-1].append(u-1)
+    res = solve(weights,uvs)
+    print(" ".join(res))
 
 if __name__ == "__main__":
     main()
