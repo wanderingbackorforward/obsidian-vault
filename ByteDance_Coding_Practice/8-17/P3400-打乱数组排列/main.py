81c34297ca146730765af21f44b1c2c7a6f36a09
ynj <yangn497@gmail.com>
2026-05-03 13:55:16 +0800

    vault backup: 2026-05-03 13:55:16
    Affected files:
    ByteDance_Coding_Practice/8-17/P3400-打乱数组排列/main.py

--- ByteDance_Coding_Practice/8-17/P3400-打乱数组排列/main.py
+++ ByteDance_Coding_Practice/8-17/P3400-打乱数组排列/main.py
@@ -1,16 +1,10 @@
 import math
 
 def main():
-    ans = []
-    T = int(input())
-    for _ in range(T):
-        n,k= map(int,input().split())
-        a = list(map(int,input().split()))
-        res=0
-        for x in a:
-            res = math.gcd(res,x)
-        ans.append(str(res*k))
-    print("\n".join(ans))
+    count = 0
+    n = int(input())
+    a = list(map(int,input().split()))
+    
     
 if __name__ == "__main__":
     main()
