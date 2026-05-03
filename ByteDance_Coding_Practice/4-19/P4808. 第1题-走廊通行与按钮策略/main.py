9c9fd5c2d337a6cb20d4fc1e8c517794f433ef5c
ynj <yangn497@gmail.com>
2026-05-03 14:53:06 +0800

    vault backup: 2026-05-03 14:53:06
    Affected files:
    .obsidian/workspace.json
    ByteDance_Coding_Practice/4-19/P3399-小红的葫芦/input.txt
    ByteDance_Coding_Practice/4-19/P3399-小红的葫芦/main.py
    ByteDance_Coding_Practice/4-19/P3399-小红的葫芦/note.md
    ByteDance_Coding_Practice/4-19/P3400-打乱数组排列/input.txt
    ByteDance_Coding_Practice/4-19/P3400-打乱数组排列/main.py
    ByteDance_Coding_Practice/4-19/P3400-打乱数组排列/note.md
    ByteDance_Coding_Practice/4-19/P3401-三元组数组/main.py
    ByteDance_Coding_Practice/4-19/P3401-三元组数组/note.md
    ByteDance_Coding_Practice/4-19/P4808. 第1题-走廊通行与按钮策略/input.txt
    ByteDance_Coding_Practice/4-19/P4808. 第1题-走廊通行与按钮策略/main.py
    ByteDance_Coding_Practice/4-19/P4808. 第1题-走廊通行与按钮策略/note.md

---
+++ ByteDance_Coding_Practice/4-19/P4808. 第1题-走廊通行与按钮策略/main.py
@@ -0,0 +1,16 @@
+import math
+
+def main():
+    ans = []
+    T = int(input())
+    for _ in range(T):
+        n,k= map(int,input().split())
+        a = list(map(int,input().split()))
+        res=0
+        for x in a:
+            res = math.gcd(res,x)
+        ans.append(str(res*k))
+    print("\n".join(ans))
+    
+if __name__ == "__main__":
+    main()
