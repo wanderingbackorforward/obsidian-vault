fdcdf9ec3ab548f319022ec4a3f1e4ec7ae45e29
ynj <yangn497@gmail.com>
2026-05-03 15:46:59 +0800

    vault backup: 2026-05-03 15:46:59
    Affected files:
    ByteDance_Coding_Practice/4-19/P4808. 第1题-走廊通行与按钮策略/main.py

--- ByteDance_Coding_Practice/4-19/P4808. 第1题-走廊通行与按钮策略/main.py
+++ ByteDance_Coding_Practice/4-19/P4808. 第1题-走廊通行与按钮策略/main.py
@@ -1,15 +1,19 @@
 import math
 
+def check(a,k,x):
+    pass
+
 def main():
     ans = []
     T = int(input())
     for _ in range(T):
+        x=0
         n,k= map(int,input().split())
         a = list(map(int,input().split()))
-        res=0
-        for x in a:
-            res = math.gcd(res,x)
-        ans.append(str(res*k))
+        while not check(a,k,x):
+            x+=1
+
+        ans.append(str(x))
     print("\n".join(ans))
     
 if __name__ == "__main__":
